466b7b40e9f9cd04c4fe5b6cd85b60651c35e8c9
mikerogers04 <mikerogers04@hotmail.com>
2023-04-03 16:23:26 -0700

    Added endpoints for index, create, update, delete CRUD actions.

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,2 +1,36 @@
 class DinosaursController < ApplicationController
+
+    def index
+        dinosaurs = Dinosaur.all 
+        render json: dinosaurs
+    end
+
+    def create
+        dinosaur = Dinosaur.create(dinosaur_params)
+        if dinosaur.valid?
+            render json: dinosaur
+        end
+    end
+
+    def update
+        dinosaur = Dinosaur.find(params[:id])
+        dinosaur.update(dinosaur_params)
+        if dinosaur.valid?
+            render json: dinosaur
+        end
+    end
+
+    def destroy
+        dinosaur = Dinosaur.find(params[:id])
+        dinosaur.destroy
+        if dinosaur.valid?
+            render json: dinosaur
+        end
+    end
+
+    #Strong params
+    private
+    def dinosaur_params
+        params.require(:dinosaur).permit(:name, :age, :diet, :enjoys, :image)
+    end
 end
